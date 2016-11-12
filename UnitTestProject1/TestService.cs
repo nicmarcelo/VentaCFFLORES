d31d5ebc375c1def19eb086bc67f6155f8cad9e5
fernandomesta <fernandomesta@gmail.com>
2016-11-12 13:54:01 -0500

    Update TestService.cs
    Actualización de ortografía

--- UnitTestProject1/TestService.cs
+++ UnitTestProject1/TestService.cs
@@ -102,15 +102,15 @@ namespace UnitTestProject1
             catch (FaultException<ProductoWSC.ProductoInexistente> error)
             {
                 if (error.Detail.exCodigo == 1)
-                    Assert.AreEqual("Para Buscar Nombre o Tipo, no debe registrar Codigo de Barras", error.Reason.ToString());
+                    Assert.AreEqual("Para Buscar Nombre o Tipo, no se debe registrar código de barras", error.Reason.ToString());
                 if (error.Detail.exCodigo == 10) 
                     Assert.AreEqual("El producto No existe", error.Reason.ToString());
                 if (error.Detail.exCodigo == 11)
                     Assert.AreEqual("El producto " + error.Detail.exProducto + " no cuenta con Stock disponible", error.Reason.ToString());
                 if (error.Detail.exCodigo == 12)
-                    Assert.AreEqual("El producto " + error.Detail.exProducto + " esta por agotarse", error.Reason.ToString());
+                    Assert.AreEqual("El producto " + error.Detail.exProducto + " está por agotarse", error.Reason.ToString());
                 if (error.Detail.exCodigo == 13)
-                    Assert.AreEqual("El producto " + error.Detail.exProducto + " esta Deshabilitado", error.Reason.ToString());
+                    Assert.AreEqual("El producto " + error.Detail.exProducto + " está deshabilitado", error.Reason.ToString());
             }
 
         }
